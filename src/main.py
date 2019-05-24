0eb6f32c0f7ed5944b16b2589f4006abba56832a
dk2n16 <david.kerr@soton.ac.uk>
2019-05-24 10:55:49 +0100

    Path dictionary modified

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from make_variables import MakeVariables 
 
 def main():
-    MakeVariables('HTI', 2012)
+    path_dict = MakeVariables('HTI', 2016)
+    print(path_dict.variables_paths['ann_composite'].exists())
+
 
 if __name__ == "__main__":
     main()
--- src/make_variables.py
+++ src/make_variables.py
@@ -41,7 +41,36 @@ class MakeVariables:
         self.BASEDIR = Path(__file__).resolve().parent.parent
         self.variables_paths = {}
         self.variables_paths.setdefault('L0', self.get_l0_path())
+        self.variables_paths.setdefault('L1', self.get_l1_path())
+        self.variables_paths.setdefault('subnational', self.get_subnational_paths())
+        self.variables_paths.setdefault('ann_composite', self.get_annual_composite_path())
+        self.variables_paths.setdefault('raw_rad9_tiles', self.get_raw_radiance_tif())
 
     def get_l0_path(self):
         """Returns path to national level shapefile"""
-        pass
+        path = self.BASEDIR.joinpath(f'shps/L0/{self.country}/{self.country.lower()}_level0_2000_2020.shp')
+        return path
+
+    def get_l1_path(self):
+        """Returns path to L1 shapefiles"""
+        path = self.BASEDIR.joinpath(f'shps/L1/{self.country}/{self.country}_adm1.shp')
+        return path
+
+    def get_subnational_paths(self):
+        """Returns paths to subnational shapefiles"""
+        paths = [x for x in self.BASEDIR.joinpath(f'shps/L2_L3_L4/{self.country}').iterdir() if x.name.endswith('shp')]
+        return paths
+
+    def get_annual_composite_path(self):
+        """Returns path to annual composite (=< 2015 is 2015 OR >= 2016 is 2016)"""
+        if self.year <= 2015:
+            path = self.BASEDIR.parent.parent.joinpath(f'download_viirs_iridis/download_viirs_iridis/datain/Annual_2015_2016/SVDNB_npp_20150101-20151231_{self.extent}_vcm-orm-ntl_v10_c201701311200.avg_rade9.tif')
+        else:
+            path = self.BASEDIR.parent.parent.joinpath(f'download_viirs_iridis/download_viirs_iridis/datain/Annual_2015_2016/SVDNB_npp_20160101-20161231_{self.extent}_vcm-orm-ntl_v10_c201807311200.avg_rade9.tif')
+        return path
+
+    def get_raw_radiance_tif(self):
+        """Returns path to raw radiance tile"""
+        path = self.BASEDIR.joinpath()
+
+    
